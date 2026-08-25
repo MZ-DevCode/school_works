5f1d0e2dbdb40483c3291be063a502f167f0dc12
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-25 20:07:35 +0200

    js

---
+++ java-script/1/script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("button");
+let result = documnet.getElementById("resultText");
+
+let click = 0;
+
+button.addEventListener('click', () =>{
+	click++
+	text.textContent = 'Click count: ' + clickCount;
+
+});

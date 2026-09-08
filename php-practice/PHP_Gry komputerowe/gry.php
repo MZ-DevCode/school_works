e12b8b46b3db6ed7922edc2eddb475e5c68fd46a
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-08 14:17:43 +0200

    connect

--- php-practice/PHP_Gry komputerowe/gry.php
+++ php-practice/PHP_Gry komputerowe/gry.php
@@ -1,11 +1,27 @@
+<?php
+    $conn = mysqli_connect("localhost, "root, "", "kino");
+?>
+
 <!DOCTYPE html>
 <html lang="pl"> //zadeklarowany polski język zawartości witryny
 <head>
     <title>Gry komputerowe</title>
 </head>
 <body>
-    <div class="naglowkowy"></div>
-    <div class="lewy"></div>
+    <div class="naglowkowy">
+        <h1>Rankieng gier komputerowych</h1>
+    </div>
+    <div class="lewy">
+        <h3>Top 5 gier w tym miesiącu</h3>
+        <ul>
+            <li>
+                <?php
+
+
+                ?>
+            </li>
+        <ul>
+    </div>
     <div class="prawy"></div>
     <main></main>
 </body>

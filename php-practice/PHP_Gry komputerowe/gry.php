65f377b2be6df995f2c15bb189b69b0585cc1bb5
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-08 14:11:04 +0200

    -

---
+++ php-practice/PHP_Gry komputerowe/gry.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl"> //zadeklarowany polski język zawartości witryny
+<head>
+    <title>Gry komputerowe</title>
+</head>
+<body>
+    <div class="naglowkowy"></div>
+    <div class="lewy"></div>
+    <div class="prawy"></div>
+    <main></main>
+</body>
+<footer>
+
+</footer>

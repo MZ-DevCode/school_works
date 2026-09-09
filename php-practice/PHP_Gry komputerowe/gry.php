ad28d350c15e78f9ba9346877fb879dbac122b5c
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-09 20:34:38 +0200

    -

--- php-practice/PHP_Gry komputerowe/gry.php
+++ php-practice/PHP_Gry komputerowe/gry.php
@@ -16,15 +16,33 @@
         <ul>
             <li>
                 <?php
-
-
+                // skrypt 1
                 ?>
             </li>
         <ul>
+            <h3>Nasz sklep</h3>
+            <a href="http://sklep.gry.pl">Tu kupisz gry/a>
     </div>
-    <div class="prawy"></div>
-    <main></main>
+    <div class="prawy">
+        <h3>Dodaj nowa gre</h3>
+        <form>
+            nazwa<input type="text" name="nazwa">
+            opis<input type="text" name="opis">
+            cena<input type="text" name="cena">
+            zdjecie<input type="text" name="zdjecie">
+            <input type="button" name="Dodaj">DODAJ
+        </form>
+    </div>
+    <main>
+        <?php
+        // skrypt 2
+        ?>
+    </main>
 </body>
 <footer>
+    <?php
+    //skrypt 3
 
+     mysqli_close($conn);
+    ?>
 </footer>

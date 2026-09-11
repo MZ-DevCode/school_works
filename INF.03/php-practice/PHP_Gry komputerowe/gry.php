06fb90736782b2155fcfd02754ad77ee57fc0b6f
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-11 18:36:15 +0200

    fix

--- INF.03/php-practice/PHP_Gry komputerowe/gry.php
+++ INF.03/php-practice/PHP_Gry komputerowe/gry.php
@@ -15,12 +15,11 @@
         <h3>Top 5 gier w tym miesiącu</h3>
         <ul>
             <?php
-                // skrypt 1
                 $query1 = "SELECT nazwa, punkty FROM gry ORDER BY punkty DESC LIMIT 5;";
                 $result1 = mysqli_query($conn, $query1);
 
                 while ($row = mysqli_fetch_object($result1)){
-                    echo "<li class='liczba-punktow'> {$row->nazwa} {$row->punkty} </li>";
+                   echo "<li> {$row->nazwa} <span class='liczba-punktow'>{$row->punkty}</span> </li>";
                 }
             ?>
         </ul>
@@ -30,13 +29,12 @@
 
     <main>
         <?php
-        // skrypt 2 (переведен на heredoc)
         $query2 = "SELECT id, nazwa, zdjecie FROM gry;";
         $result2 = mysqli_query($conn, $query2);
         while ($row = mysqli_fetch_object($result2)){
             echo <<<HTML
             <div>
-                <img src='{$row->zdjecie}' alt='{$row->nazwa}' title='{$row->id}'>
+                <img src='/pliki1/pliki1/{$row->zdjecie}' alt='{$row->nazwa}' title='{$row->id}'>
                 <p>{$row->nazwa}</p>
             </div>
             HTML;
@@ -62,7 +60,6 @@
                 <input type="submit" name="pokaz" value="Pokaż opis">
             </form>
         <?php
-        // skrypt 3
         if(isset($_POST['pokaz']) && !empty($_POST['id_gry'])) {
             $id = $_POST['id_gry'];
             $result3 = mysqli_execute_query($conn, "SELECT nazwa, punkty, cena, opis FROM gry WHERE id = ?", [$id]);

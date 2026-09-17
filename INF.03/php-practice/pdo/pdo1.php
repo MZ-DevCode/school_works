e61bf929fbae08e39de05dda436495d1acdc0549
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-17 20:16:34 +0200

    -

--- INF.03/php-practice/pdo/pdo1.php
+++ INF.03/php-practice/pdo/pdo1.php
@@ -1,22 +1,33 @@
 <?php
 
-try{
-$pdo = new PDO('sqlite:shop.db'); //подключение к бд
+try {
+    $pdo = new PDO('sqlite:shop.db'); // Подключение к бд
 
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "CREATE TABLE IF NOT EXISTS products (
+    /*
+      $pdo          - переменная с объектом подключения
+      ->            - вызов метода у объекта
+      setAttribute  - метод для установки настроек
+      PDO           - встроенный класс PHP
+      ::            - оператор доступа к константе класса
+      ATTR_ERRMODE  - настройка "режим ошибок"
+      ,             - разделитель аргументов
+      PDO::ERRMODE_EXCEPTION - значение "выбрасывать исключения при ошибках"
+    */
+
+    $sql = "CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         price REAL NOT NULL,
         stock INTEGER NOT NULL
     );";
 
-$pdo->exec($sql);
+    $pdo->exec($sql);
 
-echo "Успешное подключение";
+    echo "Успешное подключение";
 
-} catch (PDOException $e){
+} catch (PDOException $e) {
     echo "Ошибка: " . $e->getMessage();
 }
 

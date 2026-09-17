56b64a4b77d64b9b382a24ece5907a80b3fa1156
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-17 20:23:44 +0200

    -

--- INF.03/php-practice/pdo/pdo1.php
+++ INF.03/php-practice/pdo/pdo1.php
@@ -25,9 +25,22 @@ try {
 
     $pdo->exec($sql);
 
+    $stmtInsert = $pdo->prepare("INSERT INTO products (name, price, stock) VALUES (?, ?, ?)");
+    $stmtInsert->execute(["Футболка Oversize", 1499.99, 10]);
+    echo "Товар успешно добавлен<br><br>";
+    $stmtSelect = $pdo->query("SELECT id, name, price, stock FROM products");
+    $products = $stmtSelect->fetchAll(PDO::FETCH_ASSOC);
     echo "Успешное подключение";
 
-} catch (PDOException $e) {
+    echo "<b>Каталог товаров</b>"
+    foreach ($products as $product) {
+            echo "ID: {$product->id}<br>";
+            echo "Название: {$product->name}<br>";
+            echo "Цена: {$product->price}<br>";
+            echo "В наличии: {$product->stock} шт.<br>";
+        }
+
+} catch (PDOException $e) {  // PDOException - встроенный класс PHP, который ловит исключительно ошибки базы данных через PDO
     echo "Ошибка: " . $e->getMessage();
 }
 
